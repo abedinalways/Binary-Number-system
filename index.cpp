326cd82c85220981e36a3c6e9eec00dc885c7b86
abedinalways <nion.sk5067@gmail.com>
2025-01-20 12:10:24 +0600

    binary number

--- index.cpp
+++ index.cpp
@@ -1,21 +1,38 @@
+// #include <iostream>
+// using namespace std;
+// int decToBin(int decNum){
+//   int ans = 0;
+//   int pow = 1;
+//   while(decNum>0){
+//   int rem = decNum % 2;
+//   decNum /= 2;
+//     ans+= rem*pow;
+//     pow *= 10;
+//   }
+//   return ans;
+// }
+// int main(){
+// int decNum = 6;
+/// cout << decToBin(decNum) << endl;
+//   for (int i = 1; i <= 20;i++){
+//     cout << decToBin(i) << endl;
+//   }
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
-int decToBin(int decNum){
+int binToDecimal(int binNum){
   int ans = 0;
   int pow = 1;
-  while(decNum>0){
-  int rem = decNum % 2;
-  decNum /= 2;
-    ans+= rem*pow;
-    pow *= 10;
+  while(binNum>0){
+    int rem = binNum % 10;
+    ans += rem * pow;
+    binNum /= 10;
+    pow *= 2;
   }
   return ans;
 }
 int main(){
-  // int decNum = 6;
-  // cout << decToBin(decNum) << endl;
-  for (int i = 1; i <= 20;i++){
-    cout << decToBin(i) << endl;
-  }
-    return 0;
+  cout << binToDecimal(1000101) << endl;
+  return 0;
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+decimal to binary
+suppose int =69 => 1000101
+128 64 32 16 8 4 2 1
+//!SECTION two's complement 
+1. Binary form convert
+2. prefix with a 0
+3. 1's complement 
+ 0=>1
+ 1=>0
+4. add +1
